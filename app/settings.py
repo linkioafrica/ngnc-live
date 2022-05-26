20afeafb6ae28c71123a0365c976f30e0a9605d7
Unique-coder <Tom-Lesh@outlook.com>
2022-05-26 18:55:17 +0100

    ..........///.........

--- app/settings.py
+++ app/settings.py
@@ -75,7 +75,7 @@ SECURE_HSTS_SECONDS = 1
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "collectstatic")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"

40599e049efd904b6ceefbb0ee0395913b523f13
Unique-coder <Tom-Lesh@outlook.com>
2022-05-26 18:45:33 +0100

    sep6_info added

--- app/integrations/__init__.py
+++ app/integrations/__init__.py
@@ -1,9 +1,11 @@
 from .toml import toml_contents
 from .deposit import AnchorDeposit
 from .withdraw import AnchorWithdraw
+from .info import info_integration
 
 __all__ = [
     "toml_contents"
     "AnchorDeposit",
     "AnchorWithdraw",
+    "info_integration",
 ]

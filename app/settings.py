4d5b7481bac747a2c44347c30a790e2c5f7644ee
Unique-coder <Tom-Lesh@outlook.com>
2022-05-31 12:22:04 +0100

    deug set to false

--- app/settings.py
+++ app/settings.py
@@ -35,10 +35,6 @@ if os.path.exists(env_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-# ALLOWED_HOSTS = []
-
 # Production
 DEBUG = False
 ALLOWED_HOSTS = ['linkio.onrender.com']

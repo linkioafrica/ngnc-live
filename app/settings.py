b386ad404d0d0bdb6d2d349c783fcfe317ef8c8e
Unique-coder <Tom-Lesh@outlook.com>
2022-11-09 15:49:11 +0100

    setting updated

--- app/settings.py
+++ app/settings.py
@@ -73,9 +73,9 @@ MIDDLEWARE = [
 FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
-    "https://ngnc.online",
-    "http://localhost:8080",
-    "http://localhost:3080",
+    'https://ngnc.online',
+    'http://localhost:8080',
+    'http://localhost:3000',
 ]
 CORS_ALLOW_METHODS = [
 'DELETE',

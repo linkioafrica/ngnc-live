caf506b739474e1dfc3ffe9fe7d538970e3524d6
Unique-coder <Tom-Lesh@outlook.com>
2022-11-13 01:04:06 +0100

    testing CORS

--- app/settings.py
+++ app/settings.py
@@ -71,10 +71,18 @@ MIDDLEWARE = [
 ]
 
 FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
+
+CORS_ALLOW_ALL_ORIGINS  = True
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     'https://ngnc.online',
 ]
+
+CORS_ORIGIN_WHITELIST = ('https://ngnc.online',)
+
 CORS_ALLOW_METHODS = [
 'DELETE',
 'GET',
